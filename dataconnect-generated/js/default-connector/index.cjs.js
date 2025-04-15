2d7d28f56939b0796b39beccd1c5544230983ff5
takakowatabe <kikotkk@gmail.com>
2025-04-15 12:00:48 +0900

    初期設定

---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,9 @@
+const { , validateArgs } = require('firebase/data-connect');
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'namecard',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
